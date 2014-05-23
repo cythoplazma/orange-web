f73b36e4095f1e8a37dfe4966c32ad630dfab3a1
Miha Jenko <mjenko@t-2.net>
2014-05-23 13:23:27 +0200

    Auth system added.

--- app/js/app.js
+++ app/js/app.js
@@ -4,19 +4,20 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'firebase',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.animations',
-  'myApp.controllers'
+  'myApp.controllers',
 ])
 .config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/homepage', {templateUrl: 'partials/homepage.html', controller: 'ProjectCtrl'});
+  $routeProvider.when('/homepage', {templateUrl: 'partials/homepage.html', controller: 'ProjectCtrl'});
 	$routeProvider.when('/core', {templateUrl: 'partials/core.html', controller: 'CoreCtrl'});
-  	$routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
-  	$routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
-  	$routeProvider.when('/start-project', {templateUrl: 'partials/start-project.html', controller: 'StartProjectCtrl'});
-  	$routeProvider.when('/project-details', {templateUrl: 'partials/project-details.html', controller: 'ProjectCtrl'});
-  	$routeProvider.when('/usercp', {templateUrl: 'partials/usercp.html', controller: 'UserCtrl'});
-  	$routeProvider.otherwise({redirectTo: '/homepage'});
+	$routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
+	$routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
+	$routeProvider.when('/start-project', {templateUrl: 'partials/start-project.html', controller: 'StartProjectCtrl'});
+	$routeProvider.when('/project-details', {templateUrl: 'partials/project-details.html', controller: 'ProjectCtrl'});
+	$routeProvider.when('/usercp', {templateUrl: 'partials/usercp.html', controller: 'UserCtrl'});
+	$routeProvider.otherwise({redirectTo: '/homepage'});
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Services */
-angular.module('myApp.services', ['ngResource'])
+angular.module('myApp.services', ['ngResource','firebase'])
 	// .value('version', '0.1')
 	.factory('Data', ['$resource', function($resource) {
 		return $resource('data/:dataId.json', {}, {
@@ -20,4 +20,24 @@ angular.module('myApp.services', ['ngResource'])
 			// query: {method:'GET', params:{dataId:'data'}, isArray:true}
 			get: {method:"GET", params: {dataId:"users"}, isArray: true}
 		})
+	}])
+	.factory('AuthClient', ["$firebase", function($firebase) {
+		var ref = new Firebase('https://classifly.firebaseio.com');
+		return new FirebaseSimpleLogin(ref, function(error, user) {
+			if (error) {
+		    	// an error occurred while attempting login
+				console.log(error);
+				$scope.errormsg = "Failure to login!"
+				}
+			else if (user) {
+				// user authenticated with Firebase
+				console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
+				$scope.errormsg = "Success!"
+			}
+			else {
+			// user is logged out
+				$scope.errormsg = "Logged out"
+			}
+			return;
+		});
 	}]);
